46455a2985df5e62f30976c36e079b3302167b68
9d77v <9d77v@9d77v.me>
2020-04-13 00:05:42 +0800

    feat: close create episode form and refresh table

--- ui/src/profiles/desktop/settings/VideoTable.tsx
+++ ui/src/profiles/desktop/settings/VideoTable.tsx
@@ -96,6 +96,8 @@ export default function VideoTable() {
                 }
             }
         });
+        setEpisodeVisible(false);
+        await refetch()
     }
 
     const mediaData = data === undefined ? [] : data.listVideo
