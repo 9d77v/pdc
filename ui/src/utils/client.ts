e92e416fb58cac0777d42c180283732dee0ff8ca
9d77v <9d77v@9d77v.me>
2020-07-11 15:01:50 +0800

    feat: add refresh token logic

--- ui/src/utils/client.ts
+++ ui/src/utils/client.ts
@@ -1,28 +1,61 @@
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloLink, from } from 'apollo-link';
-import { onError } from 'apollo-link-error';
+import { from } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
 
+import { onError } from 'apollo-link-error';
+import jwt_decode from 'jwt-decode';
+import moment from 'moment';
+import { getRefreshToken } from '../consts/http';
+import { message as msg } from 'antd'
 const httpLink = new HttpLink({ uri: '/api' });
 
-
-const authLink = new ApolloLink((operation, forward) => {
-    // add the authorization to the headers
-    const token = localStorage.getItem('accessToken');
-    operation.setContext({
-        headers: {
-            Authorization: token ? `Bearer ${token}` : "",
+const authLink = setContext(
+    () => {
+        let token = localStorage.getItem('accessToken') || "";
+        if (token === "") {
+            return {
+                headers: {
+                    Authorization: token ? `Bearer ${token}` : "",
+                }
+            }
         }
-    });
-
-    return forward(operation);
-})
-
+        const accessToken: any = jwt_decode(token)
+        if (Number(accessToken.exp) - moment().unix() > 0) {
+            return {
+                headers: {
+                    Authorization: token ? `Bearer ${token}` : "",
+                }
+            }
+        }
+        const refreshToken = localStorage.getItem('refreshToken') || "";
+        return new Promise(async (success, fail) => {
+            //refresh token
+            const data = await getRefreshToken(refreshToken)
+            if (!data.data) {
+                success({
+                    headers: {
+                        Authorization: token ? `Bearer ${token}` : "",
+                    }
+                })
+                return
+            }
+            const refreshData: any = data.data.refreshToken
+            localStorage.setItem("accessToken", refreshData.accessToken)
+            localStorage.setItem("refreshToken", refreshData.refreshToken)
+            token = refreshData.accessToken
+            success({
+                headers: {
+                    Authorization: token ? `Bearer ${token}` : "",
+                }
+            })
+        })
+    })
 const errorLink = onError(({ graphQLErrors, networkError }) => {
     if (graphQLErrors) {
         graphQLErrors.map(({ message, locations, path }) =>
-            console.log(
+            msg.error(
                 `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
             ),
         )
@@ -31,6 +64,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
         const err: any = networkError
         if (err.statusCode === 401) {
             localStorage.clear()
+            client.resetStore()
         }
     };
 })
