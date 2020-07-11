b32d1daef6fb1c672753fd85196f664a68d3f2fb
9d77v <9d77v@9d77v.me>
2020-07-11 15:05:04 +0800

    fix: upload add token

---
+++ ui/src/consts/http.ts
@@ -0,0 +1,53 @@
+
+//getUploadURL 获取minio上传地址
+export const getUploadURL = async (bucketName: String, fileName: String) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const token = localStorage.getItem('accessToken') || "";
+    myHeaders.append("Authorization", token ? `Bearer ${token}` : "");
+
+    const graphql = JSON.stringify({
+        operationName: "presignedUrl",
+        query: ` query presignedUrl($bucketName: String!,$objectName:String!) {
+ \n    presignedUrl(bucketName: $bucketName, objectName: $objectName)
+ \n  }`,
+        variables: {
+            "bucketName": bucketName,
+            "objectName": fileName
+        }
+    })
+    const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: graphql,
+        redirect: 'follow' as const
+    };
+    const data = await fetch("/api", requestOptions)
+    return data.json()
+}
+
+export const getRefreshToken = async (refreshToken: String) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const graphql = JSON.stringify({
+        operationName: "refreshToken",
+        query: `mutation refreshToken($refreshToken:String!){
+\n            refreshToken(refreshToken:$refreshToken){
+\n               accessToken
+\n               refreshToken
+\n            }
+\n         }`,
+        variables: {
+            "refreshToken": refreshToken
+        }
+    })
+    const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: graphql,
+        redirect: 'follow' as const
+    };
+    const data = await fetch("/api", requestOptions)
+    return data.json()
+}
