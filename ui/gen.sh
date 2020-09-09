5c44a5aba64bc51e8dfea9bd45711266216a5335
9d77v <9d77v@9d77v.me>
2020-09-09 15:09:43 +0800

    feat: add proto ts

---
+++ ui/gen.sh
@@ -0,0 +1,12 @@
+PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
+ 
+# Directory to write generated code to (.js and .d.ts files) 
+OUT_DIR="./src/pb"
+ 
+protoc \
+    -I=./proto \
+    -I=/usr/local/include/google/protobuf \
+    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
+    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
+    --ts_out="${OUT_DIR}" \
+    proto/*.proto
