291563b5fd03a116f861c05ad2c43efb14c5b0ca
9d77v <9d77v@9d77v.me>
2020-10-18 11:18:13 +0800

    feat: add user page

--- middleware/user.go
+++ middleware/user.go
@@ -3,17 +3,22 @@ package middleware
 import (
 	"context"
 	"encoding/json"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
 	"strconv"
 	"strings"
+
+	"github.com/9d77v/pdc/dtos"
 )
 
 //User public user info
 type User struct {
+	UID    uint   `json:"uid"`
 	Avatar string `json:"avatar"`
 	Name   string `json:"name"`
+	Gender int    `json:"gender"`
 }
 
 //HandleCard get public user info
@@ -33,16 +38,34 @@ func HandleCard() func(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Println("get user error:", err)
 		}
-		user.Avatar = u.Avatar
+		user.UID = u.ID
+		scheme := req.Header.Get("X-Forwarded-Proto")
+		user.Avatar = dtos.GetOSSPrefix(scheme) + u.Avatar
 		user.Name = u.Name
+		user.Gender = u.Gender
 		ret, err := json.Marshal(user)
 		if err != nil {
 			log.Println("json marshal faield:", err)
 		}
-		w.Header().Set("Content-type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-type")
-		io.WriteString(w, string(ret))
+		accept := req.Header.Get("Accept")
+		if strings.Contains(accept, "application/json") {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-type")
+			w.Header().Set("Content-type", "application/json")
+			io.WriteString(w, string(ret))
+			return
+		}
+		if req.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		w.Header().Set("Content-type", "text/html")
+		tem, err := template.ParseFiles("tpls/user.html")
+		if err != nil {
+			log.Println("读取文件失败,err", err)
+			return
+		}
+		tem.Execute(w, user)
 	}
 }
