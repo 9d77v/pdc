ced1488e24af977dc483df7bb632c87d049e8b68
9d77v <9d77v@9d77v.me>
2020-12-01 21:23:03 +0800

    refactor: move subscriptions and consumers to init package

--- internal/init/init.go
+++ internal/init/init.go
@@ -2,14 +2,21 @@ package init
 
 import (
 	"log"
+	"os"
+	"os/signal"
+
+	"github.com/nats-io/stan.go"
 
 	"github.com/9d77v/pdc/internal/db/clickhouse"
 	"github.com/9d77v/pdc/internal/db/db"
+	"github.com/9d77v/pdc/internal/db/mq"
 	"github.com/9d77v/pdc/internal/db/oss"
+	device_consumers "github.com/9d77v/pdc/internal/module/device-service/consumers"
 	device "github.com/9d77v/pdc/internal/module/device-service/models"
 	history "github.com/9d77v/pdc/internal/module/history-service/models"
 	thing "github.com/9d77v/pdc/internal/module/thing-service/models"
 	user "github.com/9d77v/pdc/internal/module/user-service/models"
+	video_consumers "github.com/9d77v/pdc/internal/module/video-service/consumers"
 	video "github.com/9d77v/pdc/internal/module/video-service/models"
 	"github.com/9d77v/pdc/internal/utils"
 )
@@ -24,6 +31,8 @@ func init() {
 	new(user.User).GenerateAdminAccount(ownerName, ownerPassword)
 	clickhouse.CreateTables()
 	oss.InitMinioBuckets()
+	initSubscribe()
+	initConsumers()
 }
 
 func autoMergeTables() {
@@ -55,4 +64,47 @@ func autoMergeTables() {
 	if err != nil {
 		log.Println("auto migrate error:", err)
 	}
+}
+
+func initSubscribe() {
+	go func() {
+		qsubs := initSubScriptions()
+		interrupt := make(chan os.Signal, 1)
+		signal.Notify(interrupt, os.Interrupt)
+		<-interrupt
+		unSubscribeMQQueues(qsubs)
+	}()
+}
+
+func initSubScriptions() []stan.Subscription {
+	qsub1, err := mq.GetClient().QueueSubscribe(mq.SubjectVideo,
+		mq.GroupVideo, video_consumers.HandleVideoMSG)
+	if err != nil {
+		log.Panicln("QueueSubscribe error:", err)
+	}
+	qsub2, err := mq.GetClient().QueueSubscribe(mq.SubjectDeviceData, mq.GroupSaveDeviceData,
+		device_consumers.HandleDeviceMsg, stan.DurableName("dur"))
+	if err != nil {
+		log.Panicln("SubscribeDeviceAttribute error:", err)
+	}
+	qsub3, err := mq.GetClient().QueueSubscribe(mq.SubjectDeviceData, mq.GroupPublishDeviceData,
+		device_consumers.PublishDeviceData, stan.DurableName("dur"))
+	if err != nil {
+		log.Panicln("SubscribeDeviceAttribute error:", err)
+	}
+	return []stan.Subscription{qsub1, qsub2, qsub3}
+}
+
+func unSubscribeMQQueues(qsubs []stan.Subscription) {
+	for _, qsub := range qsubs {
+		err := qsub.Unsubscribe()
+		if err != nil {
+			log.Println("qsub Unsubscribe error:", err)
+		}
+	}
+}
+
+func initConsumers() {
+	go device_consumers.SaveDeviceTelemetry()
+	go device_consumers.SaveDeviceHealth()
 }
--- internal/module/device-service/consumers/set_telemetries_msg.go
+++ internal/module/device-service/consumers/set_telemetries_msg.go
@@ -4,14 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"time"
 
-	"github.com/9d77v/pdc/internal/db/clickhouse"
 	"github.com/9d77v/pdc/internal/db/mq"
 	"github.com/9d77v/pdc/internal/db/redis"
 	"github.com/9d77v/pdc/pkg/iot/sdk/pb"
 	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/ptypes"
 )
 
 type setTelemetriesMsg struct {
@@ -55,74 +52,5 @@ func (m *setTelemetriesMsg) sendData(deviceMsg *pb.DeviceUpMsg) {
 		if err != nil {
 			log.Printf("publish error,err:%v/n", err)
 		}
-	}
-}
-
-func (m *setTelemetriesMsg) saveDeviceTelemetry() {
-	var telemetries []*pb.Telemetry
-	timer := time.NewTimer(0)
-	if !timer.Stop() {
-		<-timer.C
-	}
-	defer timer.Stop()
-	for {
-		select {
-		case telemetry := <-telemetryChan:
-			telemetries = append(telemetries, telemetry)
-			if len(telemetries) != batchSize {
-				if len(telemetries) == 1 {
-					timer.Reset(duration)
-				}
-				break
-			}
-			batchSaveTelemetry(telemetries)
-			if !timer.Stop() {
-				<-timer.C
-			}
-			telemetries = telemetries[0:0]
-		case <-timer.C:
-			batchSaveTelemetry(telemetries)
-			telemetries = telemetries[0:0]
-		}
-	}
-}
-
-func batchSaveTelemetry(telemetries []*pb.Telemetry) {
-	tx, err := clickhouse.GetDB().Begin()
-	if err != nil {
-		log.Println("clickhouse tx begin error:", err)
-		return
-	}
-	stmt, err := tx.Prepare("INSERT INTO device_telemetry (device_id,telemetry_id,action_time,action_time_nanos, value, created_at,created_at_nanos) VALUES (?,?, ?, ?, ?,?,?)")
-	if err != nil {
-		log.Println("clickhouse tx Prepare error:", err)
-		return
-	}
-	defer stmt.Close()
-	now := time.Now()
-	for _, v := range telemetries {
-		actionTime, err := ptypes.Timestamp(v.ActionTime)
-		if err != nil {
-			log.Println("conver actiontime to timestamp error:", err)
-		}
-		if _, err := stmt.Exec(
-			v.DeviceID,
-			v.ID,
-			actionTime,
-			actionTime.Nanosecond(),
-			v.Value,
-			now,
-			now.Nanosecond(),
-		); err != nil {
-			log.Println(err)
-			err = tx.Rollback()
-			if err != nil {
-				log.Println("clickhouse tx rollback error:", err)
-			}
-			return
-		}
-	}
-	if err := tx.Commit(); err != nil {
-		log.Println("commit failed:", err)
 	}
 }
