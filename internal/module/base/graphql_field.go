408d43f4d0ff585b2826e40a41a9bc237432ceb6
9d77v <9d77v@9d77v.me>
2020-12-06 19:57:20 +0800

    refactor: refactoring NewGraphqlfField function

--- internal/module/base/graphql_field.go
+++ internal/module/base/graphql_field.go
@@ -15,30 +15,44 @@ type GraphQLField struct {
 
 //NewGraphQLField get graphql field from context
 func NewGraphQLField(ctx context.Context, prefix string) GraphQLField {
-	fieldMap := make(map[string]bool)
+	field := GraphQLField{}
 	if prefix == "" {
-		fields := graphql.CollectAllFields(ctx)
-		for _, v := range fields {
-			fieldMap[v] = true
-		}
+		field.FieldMap = getFieldMap(ctx)
 	} else {
-		fields := getPreloads(ctx)
-		for _, v := range fields {
-			if strings.HasPrefix(v, prefix) {
-				trimStr := strings.TrimPrefix(v, prefix)
-				trimArr := strings.Split(trimStr, ".")
-				fieldMap[trimArr[0]] = true
-			}
+		field.FieldMap = getFieldMapWithPrefix(ctx, prefix)
+	}
+	field.Fields = getFields(field.FieldMap)
+	return field
+}
+
+func getFieldMap(ctx context.Context) map[string]bool {
+	fieldMap := make(map[string]bool)
+	fields := graphql.CollectAllFields(ctx)
+	for _, v := range fields {
+		fieldMap[v] = true
+	}
+	return fieldMap
+}
+
+func getFieldMapWithPrefix(ctx context.Context, prefix string) map[string]bool {
+	fieldMap := make(map[string]bool)
+	fields := getPreloads(ctx)
+	for _, v := range fields {
+		if strings.HasPrefix(v, prefix) {
+			trimStr := strings.TrimPrefix(v, prefix)
+			trimArr := strings.Split(trimStr, ".")
+			fieldMap[trimArr[0]] = true
 		}
 	}
+	return fieldMap
+}
+
+func getFields(fieldMap map[string]bool) []string {
 	fields := make([]string, 0)
 	for k := range fieldMap {
 		fields = append(fields, k)
 	}
-	return GraphQLField{
-		FieldMap: fieldMap,
-		Fields:   fields,
-	}
+	return fields
 }
 
 //getPreloads ..
