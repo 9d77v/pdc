2230f9720bc10b14d63a1b6c1c4ffd4c26910ff5
9d77v <9d77v@9d77v.me>
2020-12-07 21:00:37 +0800

    refactor: extract function in video_search.go

--- internal/module/video-service/services/video_search.go
+++ internal/module/video-service/services/video_search.go
@@ -64,56 +64,19 @@ func (s VideoSearch) GetByIDFromElastic(ctx context.Context,
 
 //ListVideoIndex ..
 func (s VideoSearch) ListVideoIndex(ctx context.Context,
-	input model.SearchParam, scheme string) (int64, []*model.VideoIndex, []*model.AggResult, error) {
+	searchParam model.SearchParam, scheme string) (int64, []*model.VideoIndex, []*model.AggResult, error) {
 	boolQuery := elastic.NewBoolQuery()
-	keywordStr := strings.ReplaceAll(ptrs.String(input.Keyword), " ", "")
+	keywordStr := strings.ReplaceAll(ptrs.String(searchParam.Keyword), " ", "")
 	if keywordStr != "" {
-		subBoolQuery := elastic.NewMultiMatchQuery(keywordStr, []string{
-			"title^10",
-			"series_name^5",
-			"series_alias^0.5",
-			"desc^0.1",
-			"title.ikmax^10",
-			"series_name.ikmax^5",
-			"series_alias.ikmax^0.5",
-			"title.sy_ikmax^10",
-			"series_name.sy_ikmax^5",
-			"series_alias.sy_ikmax^0.5",
-			"title.synonym^10",
-			"series_name.synonym^5",
-			"series_alias.synonym^0.5",
-		}...).
-			Type("cross_fields").
-			Operator("AND").
-			TieBreaker(0.3)
-
-		boolQuery.Must(subBoolQuery)
+		s.buildKeywordQuery(keywordStr, boolQuery)
 	}
-	offset, limit := utils.GetElasticPageInfo(input.Page, input.PageSize)
-	filterQueries := make([]elastic.Query, 0)
-	filterQueries = append(filterQueries, elastic.NewTermQuery("is_show", true))
-
-	if len(input.Tags) > 0 {
-		for _, v := range input.Tags {
-			if v != "" {
-				filterQueries = append(filterQueries, elastic.NewTermQuery("tags", v))
-			}
-		}
-	}
-
-	ignoreQueries := make([]elastic.Query, 0)
-	if ptrs.Bool(input.IsMobile) {
-		ignoreQueries = append(ignoreQueries, elastic.NewTermQuery("is_hide_on_mobile", true))
-	}
-	filterQuery := elastic.NewBoolQuery().
-		Must(filterQueries...).MustNot(ignoreQueries...)
-	boolQuery.Filter(filterQuery)
+	boolQuery.Filter(s.buildFilterQuery(searchParam))
+	offset, limit := utils.GetElasticPageInfo(searchParam.Page, searchParam.PageSize)
 	searchService := elasticsearch.GetClient().Search().
 		Index(elasticsearch.AliasVideo).
 		Query(boolQuery).
 		From(int(offset)).
 		Size(int(limit))
-
 	aggsParams := []*es.AggsParam{
 		{Field: "tags", Size: 50},
 	}
@@ -122,7 +85,7 @@ func (s VideoSearch) ListVideoIndex(ctx context.Context,
 		searchService = es.Aggs(searchService, aggsParams...)
 	}
 	if field.FieldMap["edges"] {
-		if ptrs.Bool(input.IsRandom) {
+		if ptrs.Bool(searchParam.IsRandom) {
 			searchService.SortBy(elastic.NewScriptSort(elastic.NewScript("Math.random()"), "number").Order(true))
 		} else {
 			searchService.Sort("_score", false).
@@ -132,13 +95,60 @@ func (s VideoSearch) ListVideoIndex(ctx context.Context,
 		searchService.FetchSource(false)
 	}
 	result, err := searchService.Do(ctx)
-	vis := make([]*model.VideoIndex, 0)
-	aggResults := make([]*model.AggResult, 0)
 	if err != nil {
 		log.Println("err:", err)
-		return 0, vis, aggResults, nil
+		return 0, []*model.VideoIndex{}, []*model.AggResult{}, nil
 	}
-	if field.FieldMap["edges"] {
+	return result.TotalHits(), s.getData(field.FieldMap, result, scheme),
+		s.getAggResults(field, aggsParams, result), nil
+}
+
+func (s VideoSearch) buildKeywordQuery(keywordStr string, boolQuery *elastic.BoolQuery) {
+	subBoolQuery := elastic.NewMultiMatchQuery(keywordStr, []string{
+		"title^10",
+		"series_name^5",
+		"series_alias^0.5",
+		"desc^0.1",
+		"title.ikmax^10",
+		"series_name.ikmax^5",
+		"series_alias.ikmax^0.5",
+		"title.sy_ikmax^10",
+		"series_name.sy_ikmax^5",
+		"series_alias.sy_ikmax^0.5",
+		"title.synonym^10",
+		"series_name.synonym^5",
+		"series_alias.synonym^0.5",
+	}...).
+		Type("cross_fields").
+		Operator("AND").
+		TieBreaker(0.3)
+	boolQuery.Must(subBoolQuery)
+}
+
+func (s VideoSearch) buildFilterQuery(searchParam model.SearchParam,
+	etcIgnoreQueries ...elastic.Query) *elastic.BoolQuery {
+	filterQueries := make([]elastic.Query, 0)
+	filterQueries = append(filterQueries, elastic.NewTermQuery("is_show", true))
+	if len(searchParam.Tags) > 0 {
+		for _, v := range searchParam.Tags {
+			if v != "" {
+				filterQueries = append(filterQueries, elastic.NewTermQuery("tags", v))
+			}
+		}
+	}
+	ignoreQueries := make([]elastic.Query, 0)
+	if ptrs.Bool(searchParam.IsMobile) {
+		ignoreQueries = append(ignoreQueries, elastic.NewTermQuery("is_hide_on_mobile", true))
+	}
+	return elastic.NewBoolQuery().
+		Must(filterQueries...).
+		MustNot(append(ignoreQueries, etcIgnoreQueries...)...)
+}
+
+func (s VideoSearch) getData(fieldMap map[string]bool, result *elastic.SearchResult,
+	scheme string) []*model.VideoIndex {
+	vis := make([]*model.VideoIndex, 0)
+	if fieldMap["edges"] {
 		for _, v := range result.Hits.Hits {
 			vi := new(models.VideoIndex)
 			data, err := v.Source.MarshalJSON()
@@ -153,6 +163,12 @@ func (s VideoSearch) ListVideoIndex(ctx context.Context,
 			vis = append(vis, s.getVideoIndex(vi))
 		}
 	}
+	return vis
+}
+
+func (s VideoSearch) getAggResults(field base.GraphQLField, aggsParams []*es.AggsParam,
+	result *elastic.SearchResult) []*model.AggResult {
+	aggResults := make([]*model.AggResult, 0)
 	if field.FieldMap["aggResults"] {
 		for _, v := range aggsParams {
 			aggResult, found := result.Aggregations.Terms("group_by_" + v.Field)
@@ -166,7 +182,7 @@ func (s VideoSearch) ListVideoIndex(ctx context.Context,
 			}
 		}
 	}
-	return result.TotalHits(), vis, aggResults, nil
+	return aggResults
 }
 
 //SimilarVideoIndex ..
@@ -174,43 +190,15 @@ func (s VideoSearch) SimilarVideoIndex(ctx context.Context,
 	searchParam model.SearchParam, episodeID int64, scheme string) (int64, []*model.VideoIndex, error) {
 	videoID := models.NewEpisode().GetVideoIDByID(uint(episodeID))
 	id := strconv.FormatUint(uint64(videoID), 10)
-	vis := make([]*model.VideoIndex, 0)
-
-	boolQuery := elastic.NewBoolQuery()
-	fsQuery := elastic.NewFunctionScoreQuery()
-	mltQuery := elastic.NewMoreLikeThisQuery()
-	moreLikeThisItem := elastic.NewMoreLikeThisQueryItem()
-	moreLikeThisItem = moreLikeThisItem.Index(elasticsearch.AliasVideo).Id(id)
-	stopWords := []string{"的", "第一季", "第二季", "第三季"}
-	mltQuery = mltQuery.Field(
-		"title",
-		"desc",
-		"tags").
-		LikeItems(moreLikeThisItem).
-		MinTermFreq(1).
-		MinDocFreq(5).StopWord(
-		stopWords...,
-	).
-		MaxQueryTerms(12).
-		Analyzer("ik_smart_synonym")
 	videoDoc := new(models.VideoIndex)
 	err := videoDoc.GetByID(id)
 	if err != nil {
-		return 0, vis, nil
+		return 0, []*model.VideoIndex{}, nil
 	}
-	filterQueries := make([]elastic.Query, 0)
-	filterQueries = append(filterQueries, elastic.NewTermQuery("is_show", true))
-
-	ignoreQueries := make([]elastic.Query, 0)
 	seriesID := strconv.FormatUint(uint64(videoDoc.SeriesID), 10)
-	ignoreQueries = append(ignoreQueries, elastic.NewTermQuery("series_id", seriesID))
-	if ptrs.Bool(searchParam.IsMobile) {
-		ignoreQueries = append(ignoreQueries, elastic.NewTermQuery("is_hide_on_mobile", true))
-	}
-	filterQuery := elastic.NewBoolQuery().
-		Must(filterQueries...).
-		MustNot(ignoreQueries...)
-	boolQuery.Must(fsQuery.Query(mltQuery)).Filter(filterQuery)
+	boolQuery := elastic.NewBoolQuery().
+		Must(s.buildFunctionScoreQuery(id)).
+		Filter(s.buildFilterQuery(searchParam, elastic.NewTermQuery("series_id", seriesID)))
 	searchService := elasticsearch.GetClient().Search().
 		Index(elasticsearch.AliasVideo).
 		Query(boolQuery).
@@ -220,20 +208,21 @@ func (s VideoSearch) SimilarVideoIndex(ctx context.Context,
 	result, err := searchService.Do(ctx)
 	if err != nil {
 		log.Println("err:", err)
-		return 0, vis, nil
+		return 0, []*model.VideoIndex{}, nil
 	}
-	for _, v := range result.Hits.Hits {
-		vi := new(models.VideoIndex)
-		data, err := v.Source.MarshalJSON()
-		if err != nil {
-			log.Println("elastic search result json marshal error:", err)
-		}
-		err = json.Unmarshal(data, &vi)
-		if err != nil {
-			log.Println("elastic search result json unmarshal error:", err)
-		}
-		vi.Cover = oss.GetOSSPrefixByScheme(scheme) + vi.Cover
-		vis = append(vis, s.getVideoIndex(vi))
-	}
-	return result.TotalHits(), vis, nil
+	return result.TotalHits(), s.getData(map[string]bool{"edges": true}, result, scheme), nil
+}
+
+func (s VideoSearch) buildFunctionScoreQuery(videoID string) *elastic.FunctionScoreQuery {
+	fsQuery := elastic.NewFunctionScoreQuery()
+	moreLikeThisItem := elastic.NewMoreLikeThisQueryItem().Index(elasticsearch.AliasVideo).Id(videoID)
+	mltQuery := elastic.NewMoreLikeThisQuery().
+		Field("title", "desc", "tags").
+		LikeItems(moreLikeThisItem).
+		MinTermFreq(1).
+		MinDocFreq(5).
+		StopWord("的", "第一季", "第二季", "第三季").
+		MaxQueryTerms(12).
+		Analyzer("ik_smart_synonym")
+	return fsQuery.Query(mltQuery)
 }
