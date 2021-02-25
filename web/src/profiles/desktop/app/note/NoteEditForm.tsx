826467fbb824218a19ca63178bc73f68411b9fe0
9d77v <9d77v@9d77v.me>
2021-02-25 10:42:22 +0800

    feat: note support tex and tasklist

--- web/src/profiles/desktop/app/note/NoteEditForm.tsx
+++ web/src/profiles/desktop/app/note/NoteEditForm.tsx
@@ -2,11 +2,11 @@ import { Affix, Button, Form, Input } from 'antd'
 import moment from 'moment'
 import * as React from 'react'
 import 'src/styles/editor.less'
-import { FC, useEffect, useState } from 'react'
+import { FC, useState } from 'react'
 import { useRecoilValue, useSetRecoilState } from 'recoil'
 import noteStore from 'src/module/note/note.store'
 import {
-    UndoOutlined, RedoOutlined, CheckSquareOutlined, BoldOutlined,
+    CheckSquareOutlined, BoldOutlined,
     ItalicOutlined, StrikethroughOutlined, OrderedListOutlined, UnorderedListOutlined,
     CodeOutlined, LinkOutlined, TableOutlined, ClockCircleOutlined
 } from '@ant-design/icons';
@@ -30,8 +30,6 @@ const NoteEditForm: FC<INoteEditForm> = ({
     updateCurrentNote
 }) => {
     const buttons: IButton[] = [
-        { icon: <UndoOutlined />, type: 'undo', content: '' },
-        { icon: <RedoOutlined />, type: 'redo', content: '' },
         { icon: <CheckSquareOutlined />, type: 'over', content: '\n\n- [ ] task1\n- [x] task1\n- [ ] task1\n' },
         { text: 'H1', type: 'before', content: '# ' },
         { text: 'H2', type: 'before', content: '## ' },
@@ -58,17 +56,7 @@ const NoteEditForm: FC<INoteEditForm> = ({
     const [contentNode, setContentNode] = useState<HTMLTextAreaElement | null>(null)
     const currentNote = useRecoilValue(noteStore.currentNote)
 
-
-    const [contents, setContents] = useState<string[]>([])
-    const [contentIndex, setContentIndex] = useState(-1)
     const setNoteSyncStatus = useSetRecoilState(noteStore.noteSyncStatus)
-
-    useEffect(() => {
-        if (currentNote.content) {
-            setContents([...contents, currentNote.content])
-            setContentIndex(contentIndex + 1)
-        }
-    }, [])
 
     const [form] = Form.useForm()
     const onTitleChange = async () => {
@@ -84,8 +72,6 @@ const NoteEditForm: FC<INoteEditForm> = ({
     const onContentChange = async () => {
         setTimeout(async () => {
             const content = form.getFieldValue('content')
-            setContents([...contents, content])
-            setContentIndex(contents.length - 1)
             await noteStore.updateNoteFile(currentNote.id, currentNote.title || '', content)
             await updateCurrentNote(currentNote.id, true)
             setNoteSyncStatus(SyncStatus.Unsync)
@@ -98,24 +84,6 @@ const NoteEditForm: FC<INoteEditForm> = ({
             const type = buttons[index].type
             let buttonContent = buttons[index].content
             let content = ''
-            if (type === 'undo') {
-                setContentIndex(contentIndex - 1)
-                content = contents[contentIndex]
-                form.setFieldsValue({
-                    content
-                })
-                await updateCurrentNote(currentNote.id, true)
-                return
-            }
-            if (type === 'redo') {
-                setContentIndex(contentIndex + 1)
-                content = contents[contentIndex]
-                form.setFieldsValue({
-                    content
-                })
-                await updateCurrentNote(currentNote.id, true)
-                return
-            }
             if (type === 'clock-circle') {
                 buttonContent += moment().format("YYYY-MM-DD HH:mm:ss")
             }
@@ -149,19 +117,15 @@ const NoteEditForm: FC<INoteEditForm> = ({
                         data.substring(contentNode.selectionEnd, data.length)
                 }
             }
-            setContents([...contents, content])
-            setContentIndex(contents.length - 1)
             form.setFieldsValue({
                 content
             })
-            await updateCurrentNote(currentNote.id, true)
+            await onContentChange()
         }
     }
 
     const icons = buttons.map((v, i) => {
         return <Button key={i} icon={v.icon} onClick={onButtonClick.bind(this, i)}
-            disabled={v.type === "undo" ? (contentIndex > 0 && contents.length > 1 ? false : true) :
-                (v.type === "redo" ? (contentIndex + 1 < contents.length && contents.length > 1 ? false : true) : false)}
             style={{
                 width: 32, height: 32, fontWeight: 500,
                 justifyContent: 'center', alignItems: 'center',
@@ -170,41 +134,41 @@ const NoteEditForm: FC<INoteEditForm> = ({
     })
 
     return (
-        <Affix offsetTop={64}>
-            <div style={{ display: 'flex', width: '100%' }}>
-                <Form
-                    form={form}
-                    name="noteEditForm"
-                    initialValues={{ title: currentNote.title, content: currentNote.content }}
-                    style={{ width: "50%", marginLeft: 10, marginRight: 10 }}
+        <div style={{ display: 'flex' }}>
+            <Form
+                form={form}
+                name="noteEditForm"
+                initialValues={{ title: currentNote.title, content: currentNote.content }}
+                style={{ width: "50%", marginLeft: 10, marginRight: 10 }}
+            >
+                <Form.Item
+                    name="title"
+                    rules={[{ required: true, message: '请输入标题!' }, {
+                        max: 50, message: '标题最多50字'
+                    }]}
                 >
-                    <Form.Item
-                        name="title"
-                        rules={[{ required: true, message: '请输入标题!' }, {
-                            max: 50, message: '标题最多50字'
-                        }]}
-                    >
-                        <Input placeholder="标题" onChange={onTitleChange} />
-                    </Form.Item>
+                    <Input placeholder="标题" onChange={onTitleChange} />
+                </Form.Item>
+                <Affix offsetTop={64}>
                     {icons}
-                    <Form.Item
-                        name="content"
-                        rules={[{ required: true, message: '请输入内容!' }, {
-                            max: 10000, message: '内容最多10000字'
-                        }]}
-                    >
-                        <textarea
-                            style={{ width: "100%" }}
-                            ref={node => setContentNode(node)}
-                            placeholder="内容" rows={30} id='note-edit-form-content'
-                            onChange={onContentChange} />
-                    </Form.Item>
-                </Form>
-                <div style={{ flex: 1 }} >
-                    <NotePage />
-                </div>
-            </div >
-        </Affix>
+                </Affix>
+                <Form.Item
+                    name="content"
+                    rules={[{ required: true, message: '请输入内容!' }, {
+                        max: 10000, message: '内容最多10000字'
+                    }]}
+                >
+                    <textarea
+                        style={{ width: "100%" }}
+                        ref={node => setContentNode(node)}
+                        placeholder="内容" rows={30} id='note-edit-form-content'
+                        onChange={onContentChange} />
+                </Form.Item>
+            </Form>
+            <div style={{ width: "50%" }} >
+                <NotePage />
+            </div>
+        </div >
     )
 }
 
